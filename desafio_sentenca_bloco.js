3a5f8e9b34c1cba105b57ce5ccec6141ef6b2fcd
matheussouzapompeu <matheusducontra@gmail.com>
2024-09-03 21:35:41 -0300

    Curso Web Design

---
+++ desafio_sentenca_bloco.js
@@ -0,0 +1,17 @@
+{
+    console.log("Numeros Impares");
+    console.log("Passo #01");
+    console.log("Passo #03");
+    console.log("Passo #05");
+    console.log("Passo #07");
+    console.log("Passo #09");
+}
+
+{
+    console.log("Numeros Pares");
+    console.log("Passo #02");
+    console.log("Passo #04");
+    console.log("Passo #06");
+    console.log("Passo #08");
+    console.log("Passo #10");
+}
